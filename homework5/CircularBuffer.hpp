07eb31e16e66274f90807891804208418585d02a
Anny-waay <79156521+Anny-waay@users.noreply.github.com>
2021-05-18 13:00:20 +0300

    Update CircularBuffer.hpp

--- homework5/CircularBuffer.hpp
+++ homework5/CircularBuffer.hpp
@@ -88,7 +88,7 @@ public:
 	
 	void addLast(T x) {
 		if (size_ == capacity_) {
-			if (tail_ == 1) {
+			if (tail_ == capacity_) {
 				T* dat = new T[capacity_];
 				for (int i = 0; i < size_-1; ++i) {
 					dat[i] = data_[i+1];
@@ -105,7 +105,7 @@ public:
 			data_[size_] = x;
 			++size_;
 		}
-		tail_=1;
+		tail_=size_;
 	}
 
 	void delLast() {
