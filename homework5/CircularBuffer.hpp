55b8170cf72565acb9f469a558de9ee840a582de
Anny-waay <79156521+Anny-waay@users.noreply.github.com>
2021-05-25 00:13:05 +0300

    Update CircularBuffer.hpp

--- homework5/CircularBuffer.hpp
+++ homework5/CircularBuffer.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <exception>
+#include <sstream>
 
 template<class T>
 class CircularBuffer {
@@ -8,16 +9,37 @@ private:
 	T* data_;
 	int capacity_;
 	int size_;
-	int tail_;
+	size_t head_;
+	size_t tail_;
+
+	void incTail() {
+		if (tail_ == capacity_ - 1)
+			tail_ = 0;
+		else ++tail_;
+	};
+	void decTail() {
+		if (tail_ == 0)
+			tail_ = capacity_ - 1;
+		else --tail_;
+	};
+	void incHead() {
+		if (head_ == capacity_ - 1)
+			head_ = 0;
+		else ++head_;
+	};
+	void decHead() {
+		if (head_ == 0)
+			head_ = capacity_ - 1;
+		else --head_;
+	};
+
 public:
 	CircularBuffer(int capacity) {
 		data_ = new T[capacity];
 		capacity_ = capacity;
 		size_ = 0;
+		head_ = 0;
 		tail_ = 0;
-		for (int i = 0; i < capacity_; ++i) {
-			data_[i] = 0;
-		}
 	}
 	~CircularBuffer() {
 		delete[] data_;
@@ -28,6 +50,10 @@ public:
 	class BufferIterator {
 	private:
 		T* iterator;
+		T* _data;
+		size_t _tail;
+		size_t _index;
+		size_t _capacity;
 	public:
 		using iterator_category = std::random_access_iterator_tag;
 		using value_type = T;
@@ -36,40 +62,46 @@ public:
 		using reference = value_type&;
 
 		BufferIterator() : iterator(nullptr) {}
-		BufferIterator(T* it) : iterator(it) {}
-		BufferIterator(const BufferIterator& other) : iterator(other.iterator) {}
-		T& operator*() const { return *iterator; }
+		BufferIterator(T* it, T* buffer, size_t tail, size_t index, size_t capacity) : iterator(it), _data(buffer), _tail(tail), _index(index), _capacity(capacity) {}
+		BufferIterator(const BufferIterator& other) : iterator(other.iterator), _data(other._data), _tail(other._tail), _index(other._index), _capacity(other._capacity) {}
+		T& operator*() const { return _data[(_tail - _index) % _capacity]; }
 		T* operator->() const { return iterator; }
 		T& operator[](difference_type i) const { return iterator[i]; }
 		BufferIterator& operator ++() {
 			++iterator;
+			++_index;
 			return *this;
 		}
 		BufferIterator operator++(T) {
-			BufferIterator temp(*this);
+			BufferIterator tmp(*this);
 			++iterator;
-			return temp;
+			++_index;
+			return tmp;
 		}
 		BufferIterator& operator--() {
 			--iterator;
+			--_index;
 			return *this;
 		}
 		BufferIterator operator--(T) {
 			BufferIterator tmp(*this);
 			--iterator;
+			--_index;
 			return tmp;
 		}
 		BufferIterator& operator+=(difference_type it) {
 			iterator += it;
+			_index += it;
 			return *this;
 		}
 		BufferIterator& operator-=(difference_type it) {
 			iterator -= it;
+			_index -= it;
 			return *this;
 		}
-		BufferIterator operator+(difference_type it) const { return BufferIterator(iterator + it); }
+		BufferIterator operator+(difference_type it) const { return BufferIterator(iterator + it, _data, _tail, _index + it, _capacity); }
 		difference_type operator-(const BufferIterator& it) const { return iterator - it.iterator; }
-		BufferIterator operator-(difference_type it) const { return BufferIterator(iterator - it); }
+		BufferIterator operator-(difference_type it) const { return BufferIterator(iterator - it, _data, _tail, _index - it, _capacity); }
 		bool operator==(const BufferIterator& other) const { return iterator == other.iterator; }
 		bool operator!=(const BufferIterator& other) const { return iterator != other.iterator; }
 		bool operator>(const BufferIterator& other) const { return iterator > other.iterator; }
@@ -79,25 +111,27 @@ public:
 	};
 
 	BufferIterator begin() const {
-		return BufferIterator(data_);
+		if (tail_ != 0)
+			return BufferIterator(&data_[capacity_ - size_], data_, tail_ - 1, 0, capacity_);
+		else
+			return BufferIterator(&data_[capacity_ - size_], data_, capacity_ - 1, 0, capacity_);
 	}
 
 	BufferIterator end() const {
-		return BufferIterator(data_ + size_);
+		if (tail_ != 0)
+			return BufferIterator(&data_[capacity_], data_, tail_ - 1, 0, capacity_);
+		else
+			return BufferIterator(&data_[capacity_], data_, capacity_ - 1, 0, capacity_);
 	}
 
 	void addLast(T x) {
-		if (tail_ >= capacity_) {
-			tail_ = 0;
-		}
-		if (size_ == capacity_) {
-			data_[tail_] = x;
-		}
+		if (size_ == capacity_)
+			data_[head_] = x;
 		else {
-			data_[size_] = x;
 			++size_;
+			decHead();
+			data_[head_] = x;
 		}
-		++tail_;
 	}
 
 	void delLast() {
@@ -105,30 +139,19 @@ public:
 			throw std::out_of_range("No elements in CircularBuffer");
 		}
 		--size_;
-		data_[size_] = 0;
-		--tail_;
+		incHead();
 	}
 
-	//todo O(1)?
+	//fixed O(1)
 	void addFirst(T x) {
+		data_[tail_] = x;
 		if (size_ == capacity_) {
-			T* dat = new T[size_];
-			dat[0] = x;
-			for (int i = 1; i < size_; ++i) {
-				dat[i] = data_[i - 1];
-			}
-			delete[] data_;
-			data_ = dat;
+			incTail();
+			incHead();
 		}
 		else {
-			T* dat = new T[capacity_];
-			dat[0] = x;
-			for (int i = 1; i <= size_; ++i) {
-				dat[i] = data_[i - 1];
-			}
-			delete[] data_;
-			data_ = dat;
 			++size_;
+			incTail();
 		}
 	}
 
@@ -136,31 +159,27 @@ public:
 		if (size_ == 0) {
 			throw std::out_of_range("No elements in CircularBuffer");
 		}
-		T* dat = new T[capacity_];
-		for (int i = 0; i < size_ - 1; ++i) {
-			dat[i] = data_[i + 1];
-		}
-		delete[] data_;
-		data_ = dat;
 		--size_;
-		--tail_;
+		decTail();
 	}
 
 	const T& first() {
 		if (size_ == 0) {
 			throw std::out_of_range("No elements in CircularBuffer");
 		}
-		return data_[0];
+		if (tail_ != 0)
+			return data_[tail_ - 1];
+		else return data_[capacity_ - 1];
 	}
 
 	const T& last() {
 		if (size_ == 0) {
 			throw std::out_of_range("No elements in CircularBuffer");
 		}
-		return data_[size_ - 1];
+		return data_[head_];
 	}
 
-	//todo infomation index and size
+	//fixed infomation index and size
 	T operator[] (int index) const {
 		if (size_ == 0) {
 			throw std::out_of_range("No elements in CircularBuffer");
@@ -169,9 +188,13 @@ public:
 			throw (std::out_of_range("Incorrect index, index must be >=0"));
 		}
 		if (index > size_ - 1) {
-			throw (std::out_of_range("Index is out of range"));
+			std::stringstream ss;
+			ss << "Index = " << index << " is out of range, it must be < size(" << size_ << ")";
+			std::string s = ss.str();
+			const char* error = s.c_str();
+			throw (std::out_of_range(error));
 		}
-		return data_[index];
+		return data_[(tail_ - index - 1) % capacity_];
 	}
 
 	T& operator[](int index) {
@@ -182,9 +205,13 @@ public:
 			throw (std::out_of_range("Incorrect index, index must be >=0"));
 		}
 		if (index > size_ - 1) {
-			throw (std::out_of_range("Index is out of range"));
+			std::stringstream ss;
+			ss << "Index = " << index << " is out of range, it must be < size(" << size_ << ")";
+			std::string s = ss.str();
+			const char* error = s.c_str();
+			throw (std::out_of_range(error));
 		}
-		return data_[index];
+		return data_[(tail_ - index - 1) % capacity_];
 	}
 
 	void changeCapacity(int nc) {
@@ -192,12 +219,26 @@ public:
 			throw std::bad_alloc();
 		}
 		T* dat = new T[nc];
-		for (int i = 0; i < size_; ++i) {
-			dat[i] = data_[i];
+		int temp = 0;
+		if (head_ >= tail_) {
+			for (int i = head_; i < capacity_; ++i) {
+				dat[temp] = data_[i];
+				++temp;
+			}
+			for (int i = 0; i < tail_; ++i) {
+				dat[temp] = data_[i];
+				++temp;
+			}
 		}
+		else
+			for (int i = head_; i < tail_; ++i) {
+				dat[temp] = data_[i];
+				++temp;
+			}
 		delete[] data_;
 		data_ = dat;
 		capacity_ = nc;
+		head_ = 0;
 		tail_ = size_;
 	}
 };
